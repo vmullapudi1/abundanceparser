0e6e038383d1df408e149c51440547d2f66d30a9
Vish Mullapudi <vishruth.mullapudi@utsouthwestern.edu>
2019-07-29 16:53:57 -0500

    updated fasta file input

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+from collections import namedtuple
 from typing import List
 from Bio import SeqIO, SeqRecord
 import pandas as pd
 import toml
 
 # Path to configuration toml file
-config_file:str = "tests/testConfig.toml"
+config_file: str = "tests/testConfig.toml"
+
+FileTuple = namedtuple("FileTuple", ['FileName', 'FileData'])
+
 
 def main():
     # Get the desired configuration
@@ -14,54 +18,58 @@ def main():
     protein_fasta_files: List[str] = configuration['input']['prot_seq_fasta']
     # Internalize the file's data
     # input_data is a list of tuples (filename, pandas DataFrame of the csv)
-    input_data: list = ingestfiledata(files=input_files)
-    protein_seqrecords=getproteinsequences(protein_fasta_files)
-    data = (genrawsequences(file_tuple) for file_tuple in input_data)
+    input_data: List[FileTuple] = ingestfiledata(files=input_files)
+    protein_seqrecords: List[SeqRecord] = getproteinsequences(protein_fasta_files)
+    data = (genrawsequences(ftuple) for ftuple in input_data)
 
-    # if use_mod_in_master_prot:
-    #     localizeddata=(parsemasterlocalizations(file_tuple) for file_tuple in data)
-    # else:
-    #     localizeddata=parse
+    if use_mod_in_master_prot:
+        localized_data = (parsemasterlocalizations(ftuple) for ftuple in data)
+    else:
+        localized_data = (parseprotlocalizations(ftuple, protein_seqrecords) for ftuple in data)
 
 
-def ingestfiledata(files: List[str]) -> List[tuple]:
+def ingestfiledata(files: List[str]) -> List[FileTuple]:
     """ Takes the list if files to ingest, reads them, and returns the data as
     a list of tuples of the file name and a DataFrame of the file contents
     """
     data = []
     for input_file_path in files:
         with open(input_file_path, mode='r') as infile:
-            contents = pd.read_csv(infile,
-                                   delimiter=',',
-                                   skipinitialspace=True)
+            contents = pd.read_csv(infile, delimiter=',', skipinitialspace=True)
             contents.infer_objects()
-        data.append((infile, contents))
+        data.append(FileTuple(infile, contents))
     return data
 
 
-def getproteinsequences(fasta_files:List[str]) -> List[SeqRecord]:
-    protein_seqrecords: List[SeqRecord]=[]
+def getproteinsequences(fasta_files: List[str]) -> List[SeqRecord]:
+    protein_seqrecords: List[SeqRecord] = []
     for file in fasta_files:
         with open(file, "rU") as handle:
-            for record in SeqIO.parse(handle, "fasta"):
-                protein_seqrecords.append(record)
+            protein_seqrecords = list(SeqIO.parse(handle, "fasta"))
     return protein_seqrecords
 
 
-def genrawsequences(file_tuple):
+def genrawsequences(ftuple: FileTuple) -> FileTuple:
     """
     Adds a column to the DataFrame containing a stripped down peptide without
     the cleavage annotations
     """
-    file_data: pd.DataFrame = file_tuple[1]
+    file_data: pd.DataFrame = ftuple.FileData
     file_data = file_data.assign(
         stripped_sequence=file_data['Annotated Sequence'].apply(
-            lambda seq_str: seq_str[seq_str.find(".") + 1:seq_str.rfind(".")]))
-    return file_tuple[0], file_data
+            lambda seq_str: seq_str[seq_str.find(".") + 1: seq_str.rfind(".")]))
+    return FileTuple(ftuple.FileName, file_data)
 
 
-def parsemasterlocalizations(filedf):
-    return
+def parsemasterlocalizations(ftuple: FileTuple) -> FileTuple:
+    file_data = ftuple.FileData
+
+    pass
+
+
+def parseprotlocalizations(ftuple: FileTuple, protein_seqrecords: List[SeqRecord]) -> FileTuple:
+    # TODO
+    pass
 
 
 if __name__ == '__main__':
